6b89f7fd306c646120a6565fd649d73af7d18a9b
NAFIZ009 <jalalahmednafiz@gmail.com>
2023-10-13 22:35:24 +0600

    'post'

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
+const path = require('path');
+
 
 const server=http.createServer((req, res)=>{
     const pathObj =url.parse(req.url,true);
@@ -29,10 +32,35 @@ const server=http.createServer((req, res)=>{
             });
             req.on('end',()=>{
                 data=JSON.parse(reciveData);
-                console.log(data);
+                const filePath=path.join(__dirname,'..','todo_file',`${data.date}`);
+                fs.writeFile(filePath, JSON.stringify(data),(err)=>{
+                    if(err)
+                    {
+                        res.writeHead(500,{'content-type':'application/json'});
+                        res.end(JSON.stringify({status:'unsuccessful', message:'server error'}));
+                    }
+                    res.writeHead(201,{'content-type':'application/json'});
+                    res.end(JSON.stringify({status:'successful', message:'upload done'}));
+                });
             });
             
+        }else if(req.method=='GET')
+        {
+            const parameter=pathObj.query;
+            if(parameter.date)
+            {
+                fs.readFile(path.join(__dirname,'..','todo_file',`${parameter.date}`),'utf8',(err,data)=>{
+                    if(err)
+                    {
+                        res.writeHead(500,{'content-type':'application/json'});
+                        res.end(JSON.stringify({status:'unsuccessful', message:'server error'}));
+                    }
+                    res.writeHead(200,{'content-type':'application/json'});
+                    res.end(JSON.stringify({status:'successful', data:JSON.parse(data)}));
+                });
+            }
         }
+        
     }
 });
 
@@ -42,3 +70,11 @@ server.listen(5001,()=>{
 
 
 
+// {
+//     "date":"13-10-2023",
+//     "list":[
+//         "weak up",
+//         "code",
+//         "sleep"
+//     ]
+// }
